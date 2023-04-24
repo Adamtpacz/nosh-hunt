55fb1b95f79c7f0be99d4014d3924ced15b66aaf
Adamtpacz <adamtpacz@gmail.com>
2023-04-23 21:05:32 -0400

    Remove add comment route and extraneous lines of code

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('accounts/signup/', views.signup, name='signup'),
     path('lists/', views.lists_index, name='index'),
     path('lists/create/', views.ListCreate.as_view(), name='lists_create'),
-    path('lists/<int:list_id>', views.lists_detail, name='detail'),
-    path('lists/<int:pk>/update', views.ListUpdate.as_view(), name='lists_update'),
-    path('lists/<int:pk>/delete', views.ListDelete.as_view(), name='lists_delete'),
+    path('lists/<int:list_id>/', views.lists_detail, name='detail'),
+    path('lists/<int:pk>/update/', views.ListUpdate.as_view(), name='lists_update'),
+    path('lists/<int:pk>/delete/', views.ListDelete.as_view(), name='lists_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import List, Restaurant, Comment
-from .forms import SignupForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
